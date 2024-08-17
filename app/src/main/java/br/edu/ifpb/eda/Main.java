21413b9196240a80644595f52d9b02582ccb1eba
FihFafnir <fihfdragon@gmail.com>
2024-08-17 13:10:45 -0300

    fixed buxes

--- app/src/main/java/br/edu/ifpb/eda/Main.java
+++ app/src/main/java/br/edu/ifpb/eda/Main.java
@@ -8,16 +8,13 @@ public class Main {
     public static void main(String[] args) {
         // AVLTree<Integer> bst = new AVLTree<>();
         BinarySearchTree<Integer> bst = new AVLTree<>();
-        // bst.insert(9);
-        // bst.insert(10);
-        // bst.insert(6);
-        // bst.insert(5);
-        // bst.insert(11);
-        // bst.insert(7);
+        bst.insert(9);
+        bst.insert(10);
+        bst.insert(6);
+        bst.insert(5);
+        bst.insert(11);
+        bst.insert(7);
         bst.insert(8);
-        bst.insert(8);
-        bst.insert(8);
-        // bst.insert(8);
         System.out.println(bst);
         // bst.delete(8);
         // bst.delete(9);
